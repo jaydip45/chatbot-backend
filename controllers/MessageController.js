01482618d5b8fdf791ebac27c7d6fd768f639ea5
jaydip45 <gadhavijaydip45@gmail.com>
2025-08-07 22:22:28 +0530

    add message module

---
+++ controllers/MessageController.js
@@ -0,0 +1,33 @@
+const Message = require('../models/Message');
+const Conversation = require('../models/Conversation');
+
+const newMessage = async (req, res) => {
+  try {
+    const newMessage = new Message(req.body);
+
+    await newMessage.save();
+
+    await Conversation.findByIdAndUpdate(
+      req.body.conversationId,
+      { message: req.body.text }
+    );
+
+    return res.status(200).json('Message has been sent successfully');
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+const getMessages = async (req, res) => {
+  try {
+    const messages = await Message.find({ conversationId: req.params.id });
+    return res.status(200).json(messages);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+module.exports = {
+  newMessage,
+  getMessages
+};
--- routes/Route.js
+++ routes/Route.js
@@ -29,4 +29,7 @@ route.get('/users', getUsers);
 route.post('/conversation/add', newConversation);
 route.post('/conversation/get', getConversation);
 
+route.post('/message/add', newMessage);
+route.get('/message/get/:id', getMessages);
+
 module.exports = route;
